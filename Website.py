8b67970f31fd030a9877c1eb68f49189f52b7691
OmbudRov <meatius2222@gmail.com>
2023-08-29 18:51:05 +0530

    Added a Navigation Bar as well some styling

--- Website.py
+++ Website.py
@@ -5,5 +5,14 @@ app = Flask(__name__)
 def home():  
       return render_template('index.html')  
 
+@app.route('/Email')
+def Email():
+      return render_template('Email.html')
+@app.route('/URL')
+def URL():
+      return render_template('URL.html')
+@app.route('/Messages')
+def Messages():
+      return render_template('Messages.html')
 if __name__ == '__main__':  
    app.run(debug = True)
